2e805b272e7a585a773601681802718552a0f408
강건욱 <133373768+chamhok@users.noreply.github.com>
2023-11-16 15:04:55 +0900

    테스ㅡㅌ

--- 6Jo_Text_Game/Character.cs
+++ 6Jo_Text_Game/Character.cs
@@ -37,7 +37,8 @@
         // 기본 생성자
         public Character()
         {
-        }
+        Console.WriteLine();
+    }
 
         // 모든 속성을 초기화하는 생성자
         public Character(string name, float level, float attack, float defense, float speed, float health, int gold, Job job, Attribute attribute)
